56343d7c686a4f75a959778357aa7dcaa4861150
david-tucci <90007164+david-tucci@users.noreply.github.com>
2022-12-01 15:06:47 -0500

    Add files via upload

--- thony.py
+++ thony.py
@@ -3,6 +3,8 @@ import numpy as np
 import RPi.GPIO as GPIO
 from bokeh.plotting import figure, show
 import tweepy
+from matplotlib.ticker import MultipleLocator
+
 
 
 
@@ -145,8 +147,9 @@ class MAX31855Error(Exception):
 
 if __name__ == "__main__":
 
-    # Multi-chip example
+    # Multi-chip 
     import time
+    from time import strftime,localtime
     cs_pins = [4,17,18,24]
     clock_pin = 23
     data_pin = 22
@@ -158,26 +161,25 @@ if __name__ == "__main__":
         thermocouples.append(MAX31855(cs_pin, clock_pin, data_pin, units))
     running = True
     
-#    Note if one would like to add a thermocouple. Change the code by adding a row to matrix, add an elif statement
-#    and change the last elif to 5, as well add a list before hand and add which cs_pin it is in.
+#    Note if one would like to add a thermocouple. Add a row to matrix, add an elif statement
+#    and change the last elif to 5. Don't forget to add which cs_pin it is in.
     
     
-#   columns is the temperature every second
+#   columns is the temperature every minute
 #   rows is the amount of thermocouples
     matrix = [[0 for columns in range(1)] for rows in range(4)]
     
 #   i is the amount of thermocouples
-    i = 0 #steps
+    i = 0 #start
     
-#   seconds updates the time    
-    seconds =0
-    list_seconds=[0]
+#   minutes updates the time (its an estimate)    
+    minutes = 0
+    list_mins = [0]
     
     while(running):
-        # This stops the loop so we can loop for 86400 seconds which is one day
-        if seconds == 101:
-            print('A new day')
-            
+        # Next line stops the loop.
+        if minutes == 49: # 1 minute is 30 minutes add 1 for the amount of minutes you want.# 49 mins in a day
+            print('Run Complete')
             break
             
         try:
@@ -192,19 +194,19 @@ if __name__ == "__main__":
 #                 print(tc)
                 print("tc: {} and rj: {}".format(tc, rj))
                 
-                # adding temperature to the most recently added column of the matrix. Meaning this puts the
+                # Adding temperature to the most recently added column(time) of the matrix. Meaning this puts the
                 # right temperature for each of the 4 thermocouples.
                 if i == 0 : # thermocouple 1
-                    matrix[0][seconds]=tc
+                    matrix[0][minutes]=tc
                     
                 elif i == 1 : # thermocouple 2
-                    matrix[1][seconds]=tc
+                    matrix[1][minutes]=tc
                     
                 elif i == 2 : # thermocouple 3
-                    matrix[2][seconds]=tc
+                    matrix[2][minutes]=tc
                 
                 elif i == 3 : # thermocouple 4
-                    matrix[3][seconds]=tc
+                    matrix[3][minutes]=tc
                 
                 elif i == 4: # resets steps
                     i=0
@@ -212,41 +214,73 @@ if __name__ == "__main__":
                 
                 i+=1 #moves on to next thermocouple (steps)
             
-#                 print(matrix)
-            print(seconds)
-            time.sleep(1)
+
+            print(minutes)
             
-            
-#          bokeh part for plot... works well but needs wifi so very slow... 1 second equal 3 just for code
-#          then need to wait for it to load on wifi usually crashes after first 3 
-            
-            if seconds == 100:  
-                p = figure(title="Temperature of thermocouples", x_axis_label="time(s)",y_axis_label="Temperature(C)")
-                p.line(list_seconds, matrix[1], legend_label="Temperature of thermocouple 2", line_color="blue", line_width=2)
-                p.line(list_seconds, matrix[2], legend_label="Temperature of thermocouple 3", line_color="green", line_width=2)
-                p.line(list_seconds, matrix[3], legend_label="Temperature of thermocouple 4", line_color="red", line_width=2)
-                show(p)          
+            # Using twitter API from tweepy
+            CONSUMER_KEY = 'KwcLi3vRvV8vTdUvCYtr7v9Mr'
+            CONSUMER_SECRET = 'kzvsMqYfcVt9QEvQmSlVndOV6XQf8Urc3qye7hEvfbHpfwNju9'
+
+            ACCESS_KEY = '1578097015178907653-fgIqG3XkaHHGF96C21dlTOwL0MOgRJ'
+            ACCESS_SECRET = 'bnuH2EjmlEFEkph483rdxqSyUMp96dXSTNsOct5XriwwY'
+
+            auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+            auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
                 
-#             plt.plot(list_seconds,matrix[1])
-#             plt.plot(list_seconds,matrix[2])
-#             plt.plot(list_seconds,matrix[3])
-#             plt.xlabel("Time(s)")
-#             plt.ylabel("Temperature(C)")
-#             plt.show()
-            # The next 4 lines is to add seconds to update the graph.
-            # This loop adds a column in the matrix this column is the Temp for the next second.
-            for col in matrix:
-                col.append(0)
-            seconds+=1
-            list_seconds.append(seconds)
-            
+            api = tweepy.API(auth)
+                
+                
+#           Everything under this if statement is to create and send graphs to twitter  
+            if minutes%1 ==0: # 1
+                
+                if minutes == 0:
+                    current_time = time.strftime("%H:%M:%S", localtime())
+                    times = [current_time]
+                if minutes != 0:
+                    current_time = time.strftime("%H:%M:%S", localtime())
+                    times.append(current_time)
+                
+                # Changing the amount of ticks shown on x (time) axis.
+                if minutes <= 10:
+                    ticks = 1
+                    ticksminor = 1
+                else:
+                    ticks = 6 # 30mins for 6 steps = 180 mins(3hr) for total  8 ticks in a day might need to increa delay cluttered
+                    ticksminor = 1
+                    
+                
+                ml = MultipleLocator(ticks)
+                mlminor = MultipleLocator(ticksminor)
+                
+                plt.axes().xaxis.set_major_locator(ml)
+                plt.axes().xaxis.set_minor_locator(mlminor)
+
+                plt.plot(times, matrix[1], color = "red", label = "Thermocouple 1")
+                plt.plot(times, matrix[2], color = "blue", label = "Thermocouple 2")
+                plt.plot(times, matrix[3], color = "green", label = "Thermocouple 3")
+                plt.xlabel("Time(minutes)")
+                plt.ylabel("Temperature(C)")
+                plt.xticks(rotation=20)
+                plot=plt.savefig('plot.png')
+                
+                
+                # Sends to twitter
+                ret = api.media_upload(filename="plot.png", file=plot)
+                api.update_status(media_ids=[ret.media_id_string], status="Thermocouple 1 is red,\nThermocouple 2 is blue,\nThermocouple 3 is green,\n Started:" " " +str(times[0])+ " " "EST,\nCurrent Time: " " "+str(current_time)+ " ""EST")
+                
+                
+#               The next 5 lines is to add minutes and to update the graph.
+#               This loop adds a column in the matrix this column allows us to add a temperature for the next minute.
+                for col in matrix:
+                    col.append(0)
+                minutes+=1
+                list_mins.append(minutes)
+                time.sleep(1794)# 1794 because there is a delay of 6 seconds to send to twitter (I think
+                          # that it takes 6 seconds to save to the raspberry pi) 1794 seconds = 30 mins
+                
             
         except KeyboardInterrupt:
             running = False
     
     for thermocouple in thermocouples:
         thermocouple.cleanup()
-
-    
-#problem is delay time maybe up the sleep or say 1 second = 3
-# and how toexport plot onto twitter. p.show() creates html websites.
